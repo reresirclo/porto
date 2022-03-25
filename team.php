514944432292136a8f17f855c18a4392cdaf02e2
salmanseif <salman.seif@mail.sirclo.com>
2022-03-25 07:00:34 +0000

    add data salman

--- team.php
+++ team.php
@@ -198,6 +198,15 @@
                     </div>
                 </div>
             </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Salman</h5>
+                        <p class="card-text">Software Engineer</p>
+                        <a href="/detail/salman.php" class="btn btn-primary stretched-link">Detail</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
