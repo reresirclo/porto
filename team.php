42d9bef151f5c188111b4dcd8561a4e2069912a7
mrezalf0 <mochammad.zulfi@mail.sirclo.com>
2022-03-25 06:53:48 +0000

    add branch reza

--- team.php
+++ team.php
@@ -198,6 +198,15 @@
                     </div>
                 </div>
             </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Reza</h5>
+                        <p class="card-text">Software Engineer</p>
+                        <a href="/detail/reza.php" class="btn btn-primary stretched-link">Detail</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
