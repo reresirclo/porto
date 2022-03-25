bbf38ab42fc1c43224cd757ad72f110e9886112a
rere <samuel.reynaldi@sirclo.com>
2022-03-25 13:51:04 +0700

    nambah rere

--- team.php
+++ team.php
@@ -198,6 +198,15 @@
                     </div>
                 </div>
             </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Rere 4</h5>
+                        <p class="card-text">Software Engineer</p>
+                        <a href="/detail/rere.php" class="btn btn-primary stretched-link">Detail</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
