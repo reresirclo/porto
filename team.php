42744b29d54de4b4de4714dd8521ae81d45f30fa
DianAsihN <dian.asih@sirclo.com>
2022-04-05 00:54:35 +0000

    Add dian

--- team.php
+++ team.php
@@ -225,6 +225,15 @@
                     </div>
                 </div>
             </div> 
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Dian Asih</h5>
+                        <p class="card-text">Software Engineer</p>
+                        <a href="/detail/dian.php" class="btn btn-primary stretched-link">Detail</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
