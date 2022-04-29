36451cb3ef1ab1cde05d8557dc3b61c7ef2f3788
adi-pramu <moh.adi@sirclo.com>
2022-04-29 08:34:40 +0000

    feat([26851414] Git & Composer): add pramu

--- team.php
+++ team.php
@@ -225,6 +225,15 @@
                     </div>
                 </div>
             </div> 
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Pramu</h5>
+                        <p class="card-text">Junior Software Engineer</p>
+                        <a href="/detail/bayu.php" class="btn btn-primary stretched-link">Detail</a>
+                    </div>
+                </div>
+            </div> 
         </div>
     </div>
 </body>
