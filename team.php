3375214748a4437c7e805d2d873536a361e95c55
ridhotiyar <ridhoimanz@gmail.com>
2022-06-30 16:26:14 +0800

    test

--- team.php
+++ team.php
@@ -234,6 +234,15 @@
                     </div>
                 </div>
             </div> 
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Ridho</h5>
+                        <p class="card-text">Associate Software Engineer</p>
+                        <a href="/detail/fahmi.php" class="btn btn-primary stretched-link">Detail</a>
+                    </div>
+                </div>
+            </div> 
         </div>
     </div>
 </body>
